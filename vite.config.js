a32b1aa7c8c39136956ae56aede6f63c6eb8c4be
FullStackIndie <johnmurphy_21@yahoo.com>
2023-06-07 07:55:11 -0700

    updated vite config and workflow

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,17 @@
-import { defineConfig } from 'vite';
-import tsconfigPaths from 'vite-tsconfig-paths';
+import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig(({ command, mode }) => ({
   base: command === "/BossaLinda/",
-  publicDir: command === 'build' ? false : 'src/assets',
+  publicDir: command === "build" ? false : "src/assets",
 
   build: {
     brotliSize: false,
     manifest: false,
-    minify: mode === 'development' ? false : 'terser',
-    outDir: 'dist',
-    sourcemap: command === 'serve' ? 'inline' : false,
-
+    minify: mode === "development" ? false : "terser",
+    outDir: "dist",
+    sourcemap: command === "serve" ? "inline" : false,
+    chunkSizeWarningLimit: 1000,
     // rollupOptions: {
     //   output: {
     //     assetFileNames: 'clientlib-site/resources/[ext]/[name][extname]',
@@ -22,6 +22,7 @@ export default defineConfig(({ command, mode }) => ({
   },
 
   server: {
-    origin: 'http://localhost:3000',
+    origin: "http://localhost:3000",
   },
+  plugins: [tsconfigPaths()],
 }));
